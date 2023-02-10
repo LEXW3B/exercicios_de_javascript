c9c4165cf59c6cb84da0b88088f7b94a54816fd2
Alexandre <alexmanson12345@gmail.com>
2023-02-10 18:43:22 -0300

    feat: criar função de soma e retornar a resposta

---
+++ exercicio_03.js
@@ -0,0 +1,9 @@
+//Declare uma função que faça o envio de valores por parametro para usa-ĺos em uma soma e retornar o resultado
+
+function sum(a, b) {
+  let resp = a + b;
+
+  return resp;
+}
+
+console.log(sum(2, 3));
