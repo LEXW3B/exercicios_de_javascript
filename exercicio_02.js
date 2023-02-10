fad8d34c0a5ca980eed4bc855f1eb02d7d00051c
Alexandre <alexmanson12345@gmail.com>
2023-02-10 18:27:43 -0300

    feat: usando variavel e sabendo o tipo dela

---
+++ exercicio_02.js
@@ -0,0 +1,14 @@
+//Declare uma variavel e mostre o tipo dela
+//O typeof é um operador que retorna uma string indicando o tipo do valor do operando.
+
+let number = 7;
+console.log('number: ', number);
+
+console.log('type by number: ', typeof number);
+
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-');
+
+number = '7';
+console.log('number: ', number);
+
+console.log('type by number: ', typeof number);
